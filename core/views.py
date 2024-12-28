66c662e91031dc73098e76084d51ff2156d0ca4e
Claudio Benedetti <claudio.benedetti@mia-platform.eu>
2024-12-28 12:57:42 +0100

    feat: add jwt

--- core/views.py
+++ core/views.py
@@ -7,9 +7,14 @@ from django.core.serializers import serialize
 from .models import Entry, Habit
 from django.contrib.auth.models import User
 
-# TODO
-# auth from fe
-# deserialization
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import AllowAny
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
 
 @login_required
 @csrf_exempt
--- habits_tracker/urls.py
+++ habits_tracker/urls.py
@@ -17,9 +17,16 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from core.views import CreateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    
+    path("api/user/register/", CreateUserView.as_view(), name="register"),
+    path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
+    path("api-auth/", include("rest_framework.urls")),
+
     path("api/", include("core.urls")),
 ]
